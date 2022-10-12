e11928f8af1b5ec591492b37c7e05b60886a32d1
G00dspeed <Cory.Goodspeed@gmail.com>
2022-10-12 14:50:37 -0700

    Initial Commit

--- war.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-class War:
-    def __init__(self, message: discord.PartialMessage):
-        self.message_id = message.id
-        self.reactions = []
-    
-    def update_reactions(message)
